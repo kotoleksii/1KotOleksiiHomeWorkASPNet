6265b25f2c1f3584a3c20d8b97bbf46eb45152d5
kotoleksii <akkot08@gmail.com>
2021-11-29 12:47:34 +0200

    Add project files.

---
+++ WebApplication/About.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApplication.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <p>New City: </p>
+    <asp:ListBox ID="lstCities" runat="server" Rows="8" AutoPostBack="True" OnSelectedIndexChanged="lstCities_SelectedIndexChanged"></asp:ListBox>
+    <div>
+        <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSubmit" runat="server" Text="Add City" CssClass="btn btn-primary" OnClick="btnSubmit_Click" />
+    </div>
+    <div>
+        <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+    </div>
+</asp:Content>
---
+++ WebApplication/About.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //((Button)sender).CssClass = "btn btn-warning";
+
+            string cityName = txtCity.Text;
+            string alertMessage = $"Error! Sorry, {cityName} is already on the list!" +
+                $" Try adding something else!";
+
+            ListItem duplicateItem = lstCities.Items.FindByText(cityName);
+
+            if (lstCities.Items.Contains(duplicateItem) == true)
+            {
+                lblMessage.Text = alertMessage;
+            }
+            else if(txtCity.Text == string.Empty)
+            {
+                /*
+                * Alert modal
+                */
+                ClientScript.RegisterStartupScript(
+                              this.GetType(),
+                          "myalert",
+                      "alert('" + "The field is empty" + "');",
+                  true
+              );
+            }
+            else
+            {
+                lstCities.Items.Add(cityName);
+
+                txtCity.Text = "";
+                lblMessage.Text = "";
+            }
+
+        }
+
+        protected void lstCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtCity.Text = lstCities.SelectedItem.Text;
+        }
+    }
+}
